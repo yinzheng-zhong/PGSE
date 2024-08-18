6991063bcc7644050e4cf544f88630a9bc535368
Williiam Zhong <12530685+yinzheng-zhong@users.noreply.github.com>
2024-08-18 09:08:22 +0100

    Revert Back

--- src/model/xgb.py
+++ src/model/xgb.py
@@ -4,10 +4,9 @@ import numpy as np
 import pandas as pd
 import xgboost as xgb
 
-from src.dataset.loader import Loader
+import ray
 from src.model.util import is_essential_agreement, essential_agreement_cus_metric
 from src.log import logger
-from xgboost_ray import RayDMatrix, RayParams, train
 
 
 class XGBoost:
@@ -17,17 +16,15 @@ class XGBoost:
             num_nodes=1,
             num_cpu_per_node=8,
     ):
-        self.params = {
-            'objective': 'reg:squarederror',
-            'max_depth': 6,
-            'learning_rate': 0.1,
-            'nthread': num_cpu_per_node,
-        }
-
         self.boost_rounds = boost_rounds
         self.num_nodes = num_nodes
         self.num_cpu_per_node = num_cpu_per_node
-        self.max_cpu_per_actor = 8 if num_cpu_per_node > 8 else num_cpu_per_node
+
+        self.params = {
+            'objective': 'reg:squarederror',
+            'max_depth': 4,
+            'learning_rate': 0.1
+        }
 
     def run(
             self,
@@ -45,30 +42,16 @@ class XGBoost:
         :return:
         """
 
-        ray_params = None
-        # Create DMatrix for training and testing
-        if self.num_nodes > 1:
-            # Use RayDMatrix for better performance
-            dtrain = RayDMatrix(train_x, train_y)
-            dtest = RayDMatrix(test_x, test_y)
-
-            total_cpus = self.num_nodes * self.num_cpu_per_node
-            num_actors = total_cpus // self.max_cpu_per_actor
-            ray_params = RayParams(max_actor_restarts=1, num_actors=num_actors,
-                                   cpus_per_actor=self.max_cpu_per_actor)
-        else:
-            dtrain = xgb.DMatrix(train_x, label=train_y)
-            dtest = xgb.DMatrix(test_x, label=test_y)
+        dtrain = xgb.DMatrix(train_x, label=train_y)
+        dtest = xgb.DMatrix(test_x, label=test_y)
 
         # Watchlist to observe the training and testing performance
         watchlist = [(dtrain, 'train'), (dtest, 'test')]
 
         logger.info('Training the model...')
-        if self.num_nodes > 1:
-            model = train(params=self.params, dtrain=dtrain, evals=watchlist, num_boost_round=self.boost_rounds,
-                          ray_params=ray_params, custom_metric=essential_agreement_cus_metric)
-        else:
-            model = xgb.train(self.params, dtrain, self.boost_rounds, watchlist, custom_metric=essential_agreement_cus_metric)
+
+        model = xgb.train(self.params, dtrain, self.boost_rounds, watchlist,
+                              custom_metric=essential_agreement_cus_metric)
 
         # Print the final evaluation results
         evals_result = model.eval(dtest)
