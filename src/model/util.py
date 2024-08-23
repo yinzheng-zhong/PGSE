99f8ecb7ceccb1c414582b459d36c8b3e090b324
Yinzheng (William) Zhong <yinzheng.zhong@liverpool.ac.uk>
2024-08-23 09:57:40 +0100

    Better EA calculation

--- src/model/util.py
+++ src/model/util.py
@@ -6,13 +6,44 @@ import numpy as np
 def is_essential_agreement(
         label: Union[np.ndarray, list],
         predicted: Union[np.ndarray, list],
+        min_after_log2: float | None = None,
+        max_after_log2: float | None = None
 ):
+    """
+    Check if the predicted value is within the essential agreement range
+    :param label:
+    :param predicted:
+    :param min_after_log2:
+    :param max_after_log2:
+    :return:
+    """
+    ea = np.zeros(len(label), dtype=bool)
+
     if isinstance(label, list):
         label = np.array(label)
     if isinstance(predicted, list):
         predicted = np.array(predicted)
 
-    return np.abs(label - predicted) <= 1
+    for i in range(len(label)):
+        pred = predicted[i]
+        ceil = np.ceil(pred)
+        floor = np.floor(pred)
+
+        mid = (2 ** ceil + 2 ** floor) / 2
+        if 2 ** pred < mid:
+            pred = floor
+        else:
+            pred = ceil
+
+
+        if min_after_log2 is not None and label[i] <= min_after_log2:
+            ea[i] = pred <= min_after_log2
+        elif max_after_log2 is not None and label[i] >= max_after_log2:
+            ea[i] = pred >= max_after_log2
+        else:
+            ea[i] = abs(label[i] - pred) <= 1
+
+    return ea
 
 
 def essential_agreement_cus_metric(preds, dtrain):
