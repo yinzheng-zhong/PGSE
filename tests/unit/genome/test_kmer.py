77c859c92bd66141eb62ab71fdceffa416097d23
Yinzheng Zhong <yinzheng.zhong@liverpool.ac.uk>
2024-12-01 08:53:37 +0000

    Add kmer mapping test in genome kmer test suite
    This commit introduces a new unit test for the `kmer_mapping` method in the Kmer class. The test validates that the method correctly maps specified sequences to their expected values, enhancing the robustness of our genome module's testing coverage.

--- tests/unit/genome/test_kmer.py
+++ tests/unit/genome/test_kmer.py
@@ -1,11 +1,14 @@
 import unittest
+
+
 from src.genome import canonicalize
+from src.genome.kmer import Kmer
 
 
 class TestUtil(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.kmer = Kmer()
 
     def test_canonical_reverse_complement(self):
         # Test cases
@@ -21,6 +24,18 @@ class TestUtil(unittest.TestCase):
             with self.subTest(sequence=sequence):
                 result = canonicalize(sequence)
                 self.assertEqual(result, expected)
+
+    def test_kmer_mapping(self):
+        test_cases = [
+            ("aaaaaagg", 10),
+            ('gaatgcag', 43),
+            ('aaaaggct', 2),
+        ]
+
+        for sequence, expected in test_cases:
+            with self.subTest(sequence=sequence):
+                result = self.kmer.kmer_mapping(sequence)
+                self.assertEqual(result, sequence)
 
 
 if __name__ == '__main__':
