81c517358b1e322f0b00521e8b0a52908bcdf99a
Yinzheng Zhong <yinzheng.zhong@liverpool.ac.uk>
2025-03-24 11:09:23 +0000

    Fixed package discovery

--- pgse/__init__.py
+++ pgse/__init__.py
@@ -2,6 +2,4 @@ from pgse.pipeline.pgse_pipeline import Pipeline as TrainingPipeline
 from pgse.pipeline.pgse_inference_pipeline import Pipeline as InferencePipeline
 from pgse.pipeline.regular_pipline import Pipeline as PureXGBPipeline
 
-TrainingPipeline = TrainingPipeline
-InferencePipeline = InferencePipeline
-PureXGBPipeline = PureXGBPipeline
+__all__ = ["TrainingPipeline", "InferencePipeline", "PureXGBPipeline"]
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(here / "requirements.txt", encoding="utf-8") as f:
 
 setup(
     name="pgse",
-    version="0.3.1",
+    version="0.3.4",
     author="Yinzheng Zhong",
     author_email="yinzheng.zhong@liverpool.ac.uk",
     description="Progressive Genome Sequence Enhancement (PGSE)",
@@ -16,12 +16,11 @@ setup(
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/yinzheng-zhong/pgse",
-    packages=find_packages(where="pgse"),
-    package_dir={"": "pgse"},
+    packages=find_packages(include=["pgse", "pgse.*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9",
-    install_requires=requirements,  # Dependencies read from requirements.txt
+    install_requires=requirements,
 )
