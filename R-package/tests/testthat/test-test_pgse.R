57dd16852a8a5c858b46790ae2c140839026ec74
Alessandro Gerada <62601884+agerada@users.noreply.github.com>
2025-03-29 18:00:01 +0000

    new s3 class

--- R-package/tests/testthat/test-test_pgse.R
+++ R-package/tests/testthat/test-test_pgse.R
@@ -2,5 +2,5 @@ test_that("basic pgse run", {
   genomes_path <- file.path("fixtures", "genomes")
   labels_path <- file.path("fixtures", "labels.csv")
   p <- pgse(x = genomes_path, labels = labels_path)
-  expect_s3_class(p, "pgse")
+  expect_s3_class(p, "pgse_output")
 })
