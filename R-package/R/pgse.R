eea976258e96d10b72be6f6d8e16f0f131c98c1e
Alessandro Gerada <62601884+agerada@users.noreply.github.com>
2025-03-31 21:34:47 +0100

    new low level api link

--- R-package/R/pgse.R
+++ R-package/R/pgse.R
@@ -1,3 +1,52 @@
+validate_dir_paths_input <- function(x) {
+  if (length(x) == 0) {
+    stop("Input must be a directory or file/s.")
+  }
+  if (all(file.exists(x) & !dir.exists(x))) {
+    return()
+  }
+  if (dir.exists(x)) {
+    return()
+  }
+  stop("Input must be a directory or file/s.")
+}
+
+process_labels <- function(labels, x) {
+  # try(
+  #   {
+  #     if (file.exists(labels)) {
+  #       return(labels)
+  #     }
+  #     if (!file.exists(labels)) {
+  #       if (length(x) == 1 & dir.exists(x)) {
+  #         return(NULL)
+  #       }
+  #     }
+  #   }
+  # )
+  
+  if (length(x) == 1 && dir.exists(x)) {
+    print(x)
+    tryCatch(
+      {
+        if (file.exists(labels)) {
+          return(labels)
+        }
+      },
+      error = function(e) {
+        return(NULL)
+      }
+    )
+  }
+  
+  tmp_label_file <- tempfile(fileext = ".csv")
+  label_file <- data.frame(files = x,
+                             labels = labels)
+  write.csv(label_file, tmp_label_file, row.names = FALSE)
+  return(tmp_label_file)
+  
+}
+
 pgse <- function(x,
                  labels,
                  pre_kfold_info_file = NULL,
@@ -16,6 +65,13 @@ pgse <- function(x,
   
   if (!is.null(pre_kfold_info_file)) {
     stop("pre_kfold_info_file is not supported yet.")
+  }
+  
+  validate_dir_paths_input(x)
+  labels <- process_labels(labels, x)
+  
+  if (is.null(labels)) {
+    stop("Unable to process labels.")
   }
   
   pgse_module <- reticulate::import("pgse")
@@ -66,4 +122,60 @@ pgse <- function(x,
   
   class(output) <- append(class(output), "pgse_output", after = 0)
   return(output)
+}
+
+pgse_api_train <- function(x,
+                           labels,
+                           pre_kfold_info_file = NULL,
+                           save_file = "",
+                           export_file = "./default.export",
+                           k = 6,
+                           ext = 2,
+                           target = 70,
+                           features = 10000,
+                           folds = 0,
+                           ea_min = NULL,
+                           ea_max = NULL,
+                           num_rounds = 1500,
+                           lr = 0.03,
+                           dist = FALSE,
+                           nodes = 1,
+                           workers = 8,
+                           ...) {
+  
+  pgse_module <- reticulate::import("pgse")
+  
+  # shutdown ray if it is running
+  reticulate::py_run_string("import ray; ray.shutdown()")
+  
+  k <- as.integer(k)
+  ext <- as.integer(ext)
+  target <- as.integer(target)
+  features <- as.integer(features)
+  folds <- as.integer(folds)
+  num_rounds <- as.integer(num_rounds)
+  lr <- as.numeric(lr)
+  nodes <- as.integer(nodes)
+  workers <- as.integer(workers)
+  
+  pipeline <- pgse_module$TrainingPipeline(data_dir = x,
+                                           label_file = labels,
+                                           pre_kfold_info_file = pre_kfold_info_file,
+                                           save_file = save_file,
+                                           export_file = export_file,
+                                           k = k,
+                                           ext = ext,
+                                           target = target,
+                                           features = features,
+                                           folds = folds,
+                                           ea_min = ea_min,
+                                           ea_max = ea_max,
+                                           num_rounds = num_rounds,
+                                           lr = lr,
+                                           dist = dist,
+                                           nodes = nodes,
+                                           workers = workers,
+                                           ...)
+  
+  pipeline$run()
 }
--- R-package/tests/testthat/test-test_pgse.R
+++ R-package/tests/testthat/test-test_pgse.R
@@ -1,6 +1,73 @@
+test_that("test validate inputs", {
+  expect_no_error(validate_dir_paths_input("fixtures/genomes/"))
+  expect_no_error(validate_dir_paths_input("fixtures/genomes"))
+  expect_no_error(validate_dir_paths_input("fixtures/genomes/001.fna"))
+  expect_no_error(validate_dir_paths_input(list.files("fixtures/genomes/",
+                                                      full.names = TRUE)))
+  
+  expect_error(validate_dir_paths_input("fixtures/genomes/xyz.fna"))
+  expect_error(validate_dir_paths_input(c("fixtures/genomes/", "fixtures/genomes/001.fna")))
+  expect_error(validate_dir_paths_input(c("fixtures/genomes", "fixtures/genomes/001.fna")))
+})
+
+test_that("test label processing", {
+  expect_no_error(process_labels("fixtures/labels.csv", "fixtures/genomes/"))
+  expect_true(file.exists(process_labels("fixtures/labels.csv", "fixtures/genomes/")))
+  
+  converted_labels_file <- process_labels(rep(1, length(list.files("fixtures/genomes/"))),
+                                          list.files("fixtures/genomes/", full.names = TRUE))
+  expect_true(file.exists(converted_labels_file))
+  converted_labels <- read.csv(converted_labels_file)
+  expect_equal(converted_labels$labels, rep(1, length(list.files("fixtures/genomes/"))))
+  
+  expect_null(process_labels(rep(1, length(list.files("fixtures/genomes/"))),
+                             "fixtures/genomes/"))
+  expect_null(process_labels(rep(1, length(list.files("tests/testthat/fixtures/genomes/"))),
+                             "tests/testthat/fixtures/genomes/"))
+})
+
 test_that("basic pgse run", {
   genomes_path <- file.path("fixtures", "genomes")
   labels_path <- file.path("fixtures", "labels.csv")
   p <- pgse(x = genomes_path, labels = labels_path)
   expect_s3_class(p, "pgse_output")
+})
+
+test_that("vector pgse labels", {
+  genomes_path <- file.path("fixtures", "genomes")
+  labels <- read.csv(file.path("fixtures", "labels.csv"))$labels
+  print(labels)
+  process_labels(labels, genomes_path) |>
+    read.csv() |>
+    print()
+  stop()
+  p <- pgse(x = genomes_path, labels = labels)
+  expect_s3_class(p, "pgse_output")
+})
+
+test_that("pgse simple API", {
+  folds <- 2
+  genomes_path <- file.path("tests","testthat", "fixtures", "genomes")
+  labels_path <- file.path("tests", "testthat", "fixtures", "labels.csv")
+  save_file <- tempfile()
+  export_file <- tempfile(pattern = "pgse_export")
+  
+  pgse_api_train(x = genomes_path,
+                 labels = labels_path,
+                 save_file = save_file,
+                 export_file = export_file,
+                 folds = folds)
+  
+  
+  fold_indices <- seq(from=0, to=(folds-1), by=1)
+  export_file_names <- tools::file_path_sans_ext(basename(export_file))
+  
+  segment_file_names_full <- file.path(dirname(export_file),
+                                       paste0(export_file_names, "_fold_", fold_indices, ".txt"))
+  
+  model_file_names_full <- file.path(dirname(export_file),
+                                     paste0(export_file_names, "_fold_", fold_indices, ".json"))
+  
+  print(segment_file_names_full)
+  print(model_file_names_full)
 })
