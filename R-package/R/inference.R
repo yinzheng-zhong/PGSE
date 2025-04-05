8394589341b601bbc539c62700e80b2b10dbf3cf
Alessandro Gerada <62601884+agerada@users.noreply.github.com>
2025-04-05 17:47:48 +0100

    readme

--- R-package/R/inference.R
+++ R-package/R/inference.R
@@ -1,18 +1,36 @@
+#' Title
+#'
+#' @param object
+#' @param newdata
+#'
+#' @return
+#' @export
+#'
+#' @examples
 predict.pgse_model <- function(object,
                          newdata) {
   pgse_module <- reticulate::import("pgse")
-  
+
   tmp_model_file <- tempfile(fileext = ".json")
   xgboost::xgb.save(object$model, tmp_model_file)
-  
+
   tmp_segments_file <- tempfile(fileext = ".txt")
   writeLines(object$segments, tmp_segments_file)
-  
+
   pipeline <- pgse_module$InferencePipeline(model_path = tmp_model_file,
                                             segment_path = tmp_segments_file)
   pipeline$run(newdata)
 }
 
+#' Title
+#'
+#' @param object
+#' @param newdata
+#'
+#' @return
+#' @export
+#'
+#' @examples
 predict.pgse_output <- function(object,
                           newdata) {
   if (length(object$models) == 1) {
@@ -33,10 +51,10 @@ pgse_api_inference <- function(model_path,
                                files,
                                ...) {
   pgse_module <- reticulate::import("pgse")
-  
+
   # shutdown ray if it is running
   reticulate::py_run_string("import ray; ray.shutdown()")
-  
+
   pipeline <- pgse_module$InferencePipeline(model_path = model_path,
                                             segment_path = segment_path)
   pipeline$run(files)
