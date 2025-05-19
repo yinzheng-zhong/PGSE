9fd54bdceec663d5804c993c0d242c3159affa5b
Yinzheng Zhong <yinzheng.zhong@liverpool.ac.uk>
2025-05-19 08:46:10 +0100

    Update GitHub Actions workflow permissions and revert version to 0.8.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(here / "requirements.txt", encoding="utf-8") as f:
 
 setup(
     name="pgse",
-    version="0.8.1",
+    version="0.8.0",
     author="Yinzheng Zhong",
     author_email="yinzheng.zhong@liverpool.ac.uk",
     description="Progressive Genome Segment Enhancement (PGSE)",
